0e6290d0f3c842c3671093e07b639e51b92550a3
Ollivier Roberge <ollivier22@outlook.com>
2020-11-02 20:54:00 -0500

    Changes to make the current repo public.

---
+++ Commands.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Security.Cryptography;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+
+using DiscordBotGo;
+
+namespace commands {
+
+    public class Commands {
+        static DiscordClient discord;
+        [Command("alive"), Description("To check if the bot is alive.")]
+        public async Task Allo(CommandContext ctx) {
+            await ctx.RespondAsync("En vie.");
+            await ctx.RespondAsync("Alive");
+            await ctx.RespondAsync("私は日本語話せません。");
+        }
+
+        [Command("eat_potato")]
+        public async Task potato(CommandContext ctx) {
+            await ctx.RespondAsync("Oh oui!");
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().EndsWith("big"))
+                    await e.Message.RespondAsync("~~Je les aimes bien grosses~~...");
+            };
+        }
+        [Command("poll"), Description("Run a poll with reactions.")]
+        public async Task Poll(CommandContext ctx, [Description("How long should the poll last.")] TimeSpan duration, [Description("What options should people have.")] params DiscordEmoji[] options)
+        {
+            // first retrieve the interactivity module from the client
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var poll_options = options.Select(xe => xe.ToString());
+
+            // then let's present the poll
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Poll time!",
+                Description = string.Join(" ", poll_options)
+            };
+            var msg = await ctx.RespondAsync(embed: embed);
+
+            // add the options as reactions
+            for (var i = 0; i < options.Length; i++)
+                await msg.CreateReactionAsync(options[i]);
+
+            // collect and filter responses
+            var poll_result = await interactivity.CollectReactionsAsync(msg, duration);
+            var results = poll_result.Reactions.Where(xkvp => options.Contains(xkvp.Key))
+                .Select(xkvp => $"{xkvp.Key}: {xkvp.Value}");
+
+            // and finally post the results
+            await ctx.RespondAsync(string.Join("\n", results));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,43 +1,49 @@
 ﻿using System;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Linq;
+using System.Security.Cryptography;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using commands;
 
 namespace DiscordBotGo
 {
     class Program {
         static DiscordClient discord;
         static CommandsNextModule commands;
-        public class Commands {
-            [Command("alive")]
-            public async Task Allo(CommandContext ctx) {
-                await ctx.RespondAsync("En vie.");
-                await ctx.RespondAsync("Alive");
-                await ctx.RespondAsync("私は日本語話せません。");
-            }
-        }
-        
+        static string inputToken;
         static void Main(string[] args) {
+            Console.WriteLine("Please enter your token.");
+            inputToken = Console.ReadLine();
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
         static async Task MainAsync(string[] args) {
             discord = new DiscordClient(new DiscordConfiguration{
-                Token = "NzcyMjY0MTgzNjA1NjI0ODgy.X54JEQ.fl4ZKv6mp-v87la4ZQ9qO6dIdU4",
+                Token = inputToken,
                 TokenType = TokenType.Bot,
                 UseInternalLogHandler = true,
                 LogLevel = LogLevel.Debug
             });
             commands = discord.UseCommandsNext(new CommandsNextConfiguration{StringPrefix = "go "});
-            commands.RegisterCommands<Commands>();
-            //discord.MessageCreated += async e =>
-            //{
-            //    if (e.Message.Content.ToLower().StartsWith("ping"))
-            //        await e.Message.RespondAsync("pong!");
-            //};
+            commands.RegisterCommands<commands.Commands>();
+            discord.UseInteractivity(new InteractivityConfiguration {
+                // default pagination behaviour to just ignore the reactions
+                PaginationBehaviour = TimeoutBehaviour.Ignore,
+
+                // default pagination timeout to 5 minutes
+                PaginationTimeout = TimeSpan.FromMinutes(5),
+
+                // default timeout for other actions to 2 minutes
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
